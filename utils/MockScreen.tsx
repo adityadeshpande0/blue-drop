ea0bbe2a7b3bcfcfefd645889199dd8eee0af29c
adityadeshpande0 <adityadeshpande1@outlook.com>
2024-11-28 20:00:57 +0530

    Config

---
+++ utils/MockScreen.tsx
@@ -0,0 +1,12 @@
+import { SafeAreaView, Text, View } from "react-native";
+
+const MocksScreen: React.FC = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Welcome to Blue Drop !</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+export default MocksScreen;
