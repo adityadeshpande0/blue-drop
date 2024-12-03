a0898771ce2b894acd85f3b5374c2368e80e8db1
adityadeshpande0 <adityadeshpande1@outlook.com>
2024-12-04 01:29:55 +0530

    SOME FUNDAMENTAL CHNAGES

--- routes/AppNavigator.tsx
+++ routes/AppNavigator.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { Platform } from "react-native";
+import {
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  View,
+  StyleSheet,
+} from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { SafeAreaProvider } from "react-native-safe-area-context";
@@ -13,37 +19,61 @@ import LandingScreen from "../screens/LandingScreen";
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const AppNavigator: React.FC = () => {
+  const statusBarHeight =
+    Platform.OS === "android" ? StatusBar.currentHeight || 0 : 0;
+
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerShown: false,
-            gestureEnabled: Platform.OS === "ios",
-          }}
-          initialRouteName="mockscreen"
-        >
-          <Stack.Screen name="mockscreen" component={MocksScreen} />
-          <Stack.Screen
-            options={{
+      <View style={[styles.container, { marginTop: statusBarHeight }]}>
+        <StatusBar
+          translucent={true}
+          backgroundColor="transparent"
+          barStyle="dark-content"
+        />
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
               headerShown: false,
+              gestureEnabled: Platform.OS === "ios",
             }}
-            name="login"
-            component={Login}
-          />
-          <Stack.Screen
-            options={{
-              headerShown: false,
-            }}
-            name="register"
-            component={Register}
-          />
-          <Stack.Screen options={{headerShown:true}} name="Home" component={Home}/>
-          <Stack.Screen options={{headerShown:false}} name="landing" component={LandingScreen}/>
-        </Stack.Navigator>
-      </NavigationContainer>
+            initialRouteName="mockscreen"
+          >
+            <Stack.Screen name="mockscreen" component={MocksScreen} />
+            <Stack.Screen
+              options={{
+                headerShown: false,
+              }}
+              name="login"
+              component={Login}
+            />
+            <Stack.Screen
+              options={{
+                headerShown: false,
+              }}
+              name="register"
+              component={Register}
+            />
+            <Stack.Screen
+              options={{ headerShown: true }}
+              name="Home"
+              component={Home}
+            />
+            <Stack.Screen
+              options={{ headerShown: false }}
+              name="landing"
+              component={LandingScreen}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </View>
     </SafeAreaProvider>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default AppNavigator;
